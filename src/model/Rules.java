b2fda51d2e27d2730a13e9a40bafffffe0764785
cameron liu <cameronliu@camerons-MacBook-Pro.local>
2025-04-10 10:03:50 -0700

    finished rules.java constructor

--- src/model/Rules.java
+++ src/model/Rules.java
@@ -1,11 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
+
 public class Rules {
 
     private Deck deck; // the deck that each player (Hand.java) takes from in order to recieve a Card
+    private ArrayList<Hand> players; // stores all the players 
 
     public Rules() { 
         deck = new Deck();
+        players = new ArrayList<Hand>();
+
+        for (int i = 0; i < 4; i++) { 
+            // creates 4 players with full hands
+            Hand player = new Hand(); 
+            for (int j = 0; j < 11; j++) { // this gives the person their 11 cards 
+                player.addCard(deck.takeCard());
+            }
+            players.add(player);
+        }
     }
     
 
