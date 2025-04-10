16c82c087809a988383fedd0275bbafc9df5a2de
cameron liu <cameronliu@camerons-MacBook-Pro.local>
2025-04-10 09:54:55 -0700

    added rules.java, the class that acts as the model

---
+++ src/model/Rules.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Rules {
+
+    private Deck deck; // the deck that each player (Hand.java) takes from in order to recieve a Card
+
+    public Rules() { 
+        deck = new Deck();
+    }
+    
+
+
+    
+
+
+
+    
+}
