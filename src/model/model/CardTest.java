91aa966306ed3e46897fcd50a2c43437de80e5f7
Owner <Owner@MacBook-Air.local>
2025-04-14 17:17:44 -0700

    Added the JUnit tests for Hand, Deck, and Card classes

---
+++ src/model/model/CardTest.java
@@ -0,0 +1,46 @@
+package model.model;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+class CardTest {
+
+	@Test
+    public void testGetShuffledCards() {
+        // Checks that the player gets 52 shuffled cards
+        ArrayList<Card> cards = Card.getShuffledCards();
+        assertEquals(52, cards.size());
+    }
+    
+    @Test
+    public void testCardRankAndSuit() {
+        // Checks that a card has a rank and suit
+        ArrayList<Card> cards = Card.getShuffledCards();
+        Card card = cards.get(0);
+        
+        assertNotNull(card.rank);
+        assertNotNull(card.suit);
+    }
+    
+    @Test
+    public void testToString() {
+        // Checks that the toString method returns the expected string
+        ArrayList<Card> cards = Card.getShuffledCards();
+        Card card = cards.get(0);
+        
+        assertEquals(card.rank + " ", card.toString());
+    }
+    
+    @Test
+    public void testShuffling() {
+        // Generates two shuffles and checks that they are both different
+        ArrayList<Card> firstShuffle = Card.getShuffledCards();
+        ArrayList<Card> secondShuffle = Card.getShuffledCards();
+        
+        // Make sure we got two different list instances
+        assertNotSame(firstShuffle, secondShuffle);
+    }
+}
