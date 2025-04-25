57f6b38a86a153d07b2498c444d2d267ac748824
notan79 <njcrutchfield@gmail.com>
2025-04-25 14:20:27 -0700

    final commit

--- src/controller/Observer.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-/*
-    Simple interface to allow for the model to directly update the UI after
-    an action is taken to update the game.
- */
-public interface Observer {
-    void updateUI();
-}
