893d609fec04aadccf4842aac97a507fe99c7485
notan79 <njcrutchfield@gmail.com>
2025-04-25 01:27:07 -0700

    added testcase

--- test/model/StrategyTest.java
+++ test/model/StrategyTest.java
@@ -2,13 +2,33 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class StrategyTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void BestStrategy() {
+		Rules game = new Rules();
+		BestStrategy bs = new BestStrategy();
+		
+		ArrayList<Card> mh = game.getMainHand();
+		ArrayList<Card> fu = game.getFaceUpHand();
+		ArrayList<Card> fd = game.getFaceDownHand();
+		
+		ArrayList<Card> all = Card.getShuffledCards();
+		Card two = null;
+		for(Card c : all) {
+			if(c.rank == Rank.TWO) {
+				two = c;
+				break;
+			}
+		}
+		System.out.println(bs.whatCardToPlay(mh, fu, fd, two, true, true));
+		
+		
+		
 	}
 
 }
